a87f1f08004c170f2ccd1fde06fc9d3079b14ac3
Salty27 <bendhect04@gmail.com>
2025-05-07 09:37:42 +0200

    Added test file for future use

---
+++ test/VulkanTutorial/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by alejandro on 05/05/2025.
+//
+
+#include <iostream>
+
+#include "VulkanTutorial/vulkanTutorial.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    hello();
+}
